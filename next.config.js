6c7db70d3df925883c7aed378c79a2eecdcabd92
easis <erik.asis93@gmail.com>
2022-08-19 18:37:59 +0200

    Fixed asset path

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     basePath: '/nfc-uid-read',
-    assetPrefix: isProd ? '/your-github-repo-name/' : '',
+    assetPrefix: isProd ? '/nfc-uid-read/' : '',
 };
