d6c3545fb2461d4db1232f751fb14dd73f456940
easis <erik.asis93@gmail.com>
2022-08-20 04:16:51 +0200

    Updated TailwindCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -5,7 +8,27 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "primary": colors.indigo,
+                "secondary": colors.cyan,
+                "dark-gray": {
+                    "50": "#E8E8E8",
+                    "100": "#D1D1D1",
+                    "200": "#A3A3A3",
+                    "300": "#757575",
+                    "400": "#474747",
+                    "500": "#1B1B1B",
+                    "600": "#141414",
+                    "700": "#0F0F0F",
+                    "800": "#0A0A0A",
+                    "900": "#050505"
+                }
+            },
+        },
     },
     plugins: [],
 }
