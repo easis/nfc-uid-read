308cacbc830d8dae7fc84ff0d99a85aaeb4489ee
easis <erik.asis93@gmail.com>
2022-08-20 17:30:35 +0200

    Added animation to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,21 @@ module.exports = {
                     "900": "#050505"
                 }
             },
+            keyframes: {
+                'fade-in-down': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(50%)'
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)'
+                    },
+                }
+            },
+            animation: {
+                'fade-in-down': 'fade-in-down 0.3s ease-out'
+            }
         },
     },
     plugins: [],
