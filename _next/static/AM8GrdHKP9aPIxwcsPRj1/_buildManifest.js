ee766a3c6aa98cfd27fdc8001644a494482e7d85
easis <easis@users.noreply.github.com>
2022-08-20 16:20:46 +0000

    deploy: be1b82ebea32178aedeef8116181eb0cab9cb5e6

--- _next/static/AM8GrdHKP9aPIxwcsPRj1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-1fa8cbd89fe9cade.js","static/css/ac489253c690ebd6.css","static/chunks/pages/index-88dd9aab01c060ce.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
