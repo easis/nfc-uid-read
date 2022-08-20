cf78a851f33dd67e50dbe53205cea9acb4911d32
easis <erik.asis93@gmail.com>
2022-08-20 04:45:23 +0200

    Created Footer component

---
+++ components/shared/footer.tsx
@@ -0,0 +1,31 @@
+import styles from '@/components/shared/footer.module.css';
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className={styles.footer}>
+            <hr className={styles.divider} />
+
+            <div className={styles.container}>
+                <p className={styles.author} title='aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ=='>Made with ❤ by Erik Asís</p>
+
+                <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <Link href="https://github.com/easis/nfc-uid-read">
+                            <a className={styles.link} target='_blank'>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+                                </svg>
+
+                                GitHub
+                            </a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </footer >
+    );
+};
+
+export default Footer;
--- components/shared/layout.tsx
+++ components/shared/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from './footer';
 import styles from './layout.module.css';
 
 interface LayoutProps {
@@ -6,15 +7,24 @@ interface LayoutProps {
 
 const Layout = (props: LayoutProps) => {
     return (
-        <div className={styles.app}>
-            <h1 className={styles.title}>
-                NFC UID Read
-            </h1>
+        <>
+            <div className={styles.app}>
+                <h1 className={styles.title}>
+                    NFC UID Read
+                </h1>
 
-            <main className={styles.main}>
-                {props.children}
-            </main>
-        </div>
+                <main className={styles.main}>
+                    {props.children}
+                    <label htmlFor="default-toggle" className="inline-flex relative items-center mb-4 cursor-pointer">
+                        <span className="mr-3 text-sm font-medium text-gray-900 dark:text-gray-300">hex</span>
+                        <input type="checkbox" value="" id="default-toggle" className="sr-only peer" />
+                        <div className="w-9 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[3ch] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">dec</span>
+                    </label>
+                </main>
+            </div>
+            <Footer />
+        </>
     );
 };
 
