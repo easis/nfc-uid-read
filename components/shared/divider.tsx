3a444abd3dd4056fe3433c600d43a0d31e6c1c7d
easis <erik.asis93@gmail.com>
2022-08-20 17:29:44 +0200

    Created Divider component

---
+++ components/shared/divider.tsx
@@ -0,0 +1,9 @@
+import styles from '@/components/shared/divider.module.css';
+
+const Divider = () => {
+    return (
+        <hr className={styles.divider} />
+    );
+};
+
+export default Divider;
