5eb5133ebf579ac1248ada0bb12775ba87108cf3
easis <erik.asis93@gmail.com>
2022-08-20 04:16:12 +0200

    Created Layout component

---
+++ components/shared/layout.tsx
@@ -0,0 +1,21 @@
+import styles from './layout.module.css';
+
+interface LayoutProps {
+    children: JSX.Element;
+};
+
+const Layout = (props: LayoutProps) => {
+    return (
+        <div className={styles.app}>
+            <h1 className={styles.title}>
+                NFC UID Read
+            </h1>
+
+            <main className={styles.main}>
+                {props.children}
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
